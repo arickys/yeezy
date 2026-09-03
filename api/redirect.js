c20ecc7e86811ff1f89fbd0972464935f7448eab
ph-Science <nuubzz12@gmail.com>
2026-09-02 20:45:11 -0500

    hey
    hey

---
+++ api/redirect.js
@@ -0,0 +1,23 @@
+const REDIRECT_TO_HOME = true;
+
+export default function handler(req, res) {
+    if (!REDIRECT_TO_HOME) {
+        return res.status(200).json({
+            redirect: false
+        });
+    }
+
+    const path = req.url.split("?")[0];
+
+    if (path === "/") {
+        return res.status(200).json({
+            redirect: false
+        });
+    }
+
+    res.writeHead(307, {
+        Location: "/"
+    });
+
+    res.end();
+}
